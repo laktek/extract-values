d0ce83c292c651ff7eb81984690179fa404fe2ef
Lakshan Perera <lakshan@web2media.net>
2012-10-04 06:52:29 +0530

    initial commit

---
+++ extract_values.js
@@ -0,0 +1,30 @@
+var extractValues = function(str, pattern, options) {
+	options = options || {};
+	var delimeters = options.delimeters || ["{", "}"];
+	var whitespace = options.whitespace;
+
+	var special_chars_regex = /[\\\^\$\*\+\.\?\(\)]/g;
+	var token_regex = new RegExp( delimeters[0] + "([^" + delimeters.join("") + "\t\r\n]+)" + delimeters[1], "g");
+	var tokens = pattern.match(token_regex);
+	var pattern_regex = new RegExp(pattern.replace(special_chars_regex, "\\$&").replace(token_regex, "(\.+)"));
+
+	if (whitespace) {
+		str = str.replace(/\s+/g, function(match) {
+			var whitespace_str = "";
+			for (var i = 0; i < whitespace; i++) {
+				whitespace_str = whitespace_str + match.charAt(0);
+			};
+			return whitespace_str;
+		});
+	};
+
+	var matches = str.match(pattern_regex).splice(1);
+	var output = {};
+	for (var i=0; i < tokens.length; i++) {
+		output[tokens[i].replace( new RegExp( delimeters[0] + "|" + delimeters[1], "g"), "")] = matches[i];
+	}
+
+	return output;
+}
+
+module.exports = extractValues;
---
+++ tests.js
@@ -0,0 +1,22 @@
+var extract_values = require("./extract_values.js");
+var assert = require("assert");
+
+var cases = [
+	[["a:b,c:d", "a:{a},c:{c}"], { "a": "b", "c": "d" }],
+
+	[["/2012/08/12/test.html", "/{year}/{month}/{day}/{title}.html"], { "year": "2012", "month": "08", "day": "12", "title": "test" }],
+
+	[["Content-Type: text/html; charset=utf-8", "Content-Type: {mime}; charset={charset}"], {"mime": "text/html", "charset": "utf-8" }],
+
+	[["/assets/images/logo.jpg", "{dirpath}/{basename}.{extension}"], { "dirpath": "/assets/images", "basename": "logo", "extension": "jpg" }],
+
+	[["Lakshan Perera <lakshan@web2media.net> (http://laktek.com)", "{name} <{email}> ({url})"], {"name": "Lakshan Perera", "email": "lakshan@web2media.net", "url": "http://laktek.com" }],
+
+	[["a:b,c:d", "a:{{a}},c:{{c}}", { delimeters: ["{{", "}}"] }], { "a": "b", "c": "d" }],
+
+	[["red  blue   green", "{first} {second} {third}", { whitespace: 1 }], {"first": "red", "second": "blue", "third": "green" }]
+]
+
+for (var i = 0; i < cases.length; i++) {
+	assert.deepEqual(extract_values.apply(this, cases[i][0]), cases[i][1]);
+}
